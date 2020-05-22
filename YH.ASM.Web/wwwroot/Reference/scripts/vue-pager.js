8a446662b29505d62b7296a69f37107860c705aa
demon28 <274733956@qq.com>
2020-05-22 17:39:31 +0800

    初次提交

---
+++ YH.ASM.Web/wwwroot/Reference/scripts/vue-pager.js
@@ -0,0 +1,180 @@
+﻿!function (a) {
+    "wui" in window || (window.wui = {});
+}(),
+    function (a, b) {
+        if (!b) {
+            console.log("required jQuery reference");
+            return;
+        }
+        a.pagination = function (opt) {
+            var vcp = new VueChangePage(opt);
+            return vcp;
+        };
+    }(wui, jQuery);
+
+
+function VueChangePage(options) {
+    var self = this;
+    self.setting = options;
+    this.show = function () {
+        _loadData();
+        return self;
+    },
+    this.reload = function (cachecontrol) {
+            _loadData(undefined, cachecontrol);
+            return self;
+        };
+    function _pageLoading() {
+        this.show = function () {
+            $(self.setting.el).mask();
+        };
+        this.close = function () {
+            $(self.setting.el).mask("close");
+        };
+    }
+    function _complete(data) {
+        if (data && self.setting.onComplete && typeof self.setting.onComplete == 'function') {
+            self.setting.onComplete.call(data);
+        }
+    }
+    function _loadData(arg, cachecontrol) {
+        var loading = new _pageLoading();
+        if (!self.setting.loading) {
+            loading = {
+                show: function () { },
+                close: function () { }
+            };
+        }
+        loading.show();
+        var cusData = {};
+        if(typeof self.setting.query =='function'){
+            cusData = self.setting.query();
+        }else{
+            cusData = self.setting.query;
+        }
+        var ajaxdata = cusData;
+        if (arg) {
+            ajaxdata = $.extend(arg, cusData);
+            ajaxdata.pageindex = arg.pageindex || ajaxdata.pageindex || 1;
+            ajaxdata.pagesize = ajaxdata.pagesize;
+        } else {
+            ajaxdata = $.extend({ "pagesize": self.setting.pagesize, "pageindex": 1 }, cusData);
+        }
+        ajaxdata.pagesize = ajaxdata.pagesize || 10;
+        $.ajax({
+            url: self.setting.url,
+            type: "POST",
+            data: ajaxdata,
+            beforeSend: function (xhr) {
+                if (cachecontrol) {
+                    xhr.setRequestHeader("Cache-Control", "no-cache");
+                }
+            },
+            success: function (json) {
+                loading.close();
+                if (json.Success) {
+                    _refreshUI(json.Content);
+                } else {
+                    _showTips(json.Message);
+                }
+            },
+            error: function () {
+                loading.close();
+                _showTips("连接服务器失败");
+            }
+        });
+    }
+    function _showTips(message) {
+        var action = self.setting.showTips || window.alert;
+        action(message);
+    }
+    function _refreshUI(content) {
+        if (content.Data) {
+            self.Model.Data = [];
+            self.Model.Data = content.Data;
+        }
+        __render(content.PageIndex, content.Count, content.PageCount);
+        _complete(content);
+    }
+    function __render(currentIndex, totalRecords, totalPageCount) {
+        if (self.setting.enablePaginate === false) {
+            return;
+        }
+        if (totalPageCount <= 0) {
+            return;
+        }
+        var currentPageIndex = currentIndex;
+        var pagesize = self.setting.pagesize || 10;
+        var ul = $("<ul></ul>").addClass("pagination");
+        var prev = $("<li class='page-item'></li>").append($("<a class='page-link'></a>").append($("<i class='icon-double-angle-left fa fa-angle-double-left'></i>"))).addClass("prev");
+        if (currentPageIndex == 1) {
+            prev.addClass("disabled");
+        }
+
+        var next = $("<li class='page-item'></li>").append($("<a class='page-link'></a>").append($("<i class='icon-double-angle-right fa fa-angle-double-right'></i>"))).addClass("next");
+        if (totalPageCount <= 1 || currentPageIndex == totalPageCount) {
+            next.addClass("disabled");
+        }
+        var recordsinfo = $("<li class='page-item disabled'></li>").append($("<a class='page-link'></a>")
+            .append("<span>共</span><span class='totalrecords'>" + totalRecords + "</span><span>条，每页</span>" +
+                "<span class='pagesize'>" + pagesize + "</span>条，<span></span>" + "<span class='maxpageindex'>" + totalPageCount + "</span><span>页</span>"));
+
+        var minIndex = currentPageIndex - 5;
+        var plus = 0;
+        if (minIndex <= 0) {
+            plus = 0 - minIndex;
+            minIndex = 1;
+        }
+        var maxIndex = currentPageIndex + 5 + plus;
+        if (maxIndex > totalPageCount) {
+            maxIndex = totalPageCount;
+        }
+        ul.append(prev);
+        for (var i = minIndex; i <= maxIndex; i++) {
+            var li_html = $("<li class='page-item'></li>");
+            if (currentPageIndex == i)
+                li_html.addClass("active disabled");
+            var item = li_html.append($("<a class='page-link'></a>").html(i));
+            ul.append(item);
+        }
+        ul.append(next);
+        ul.append(recordsinfo);
+        //ul.append(pagesizeoptions);
+        ul.children("li").click(function () {
+            if ($(this).is(".disabled")) {
+                return;
+            }
+            var index = $(this).text();
+            if (index.length == 0) {
+                index = ul.find(".active").text();
+                if ($(this).is(".prev") && index > 1) {
+                    index--;
+                } else if ($(this).is(".next") && index < totalPageCount) {
+                    index++;
+                }
+            }
+            var arg = { "pageindex": parseInt(index), "pagesize": pagesize };
+            _loadData(arg);
+        });
+        if ($(self.setting.el).parent().find(".dataTables_paginate").length > 0) {
+            $(self.setting.el).parent().find(".dataTables_paginate").empty().append(ul);
+        } else {
+            var div_pagination = $("<div class=\"dataTables_paginate paging_bootstrap pull-right\" style=\"margin-right:20px\"></div>");
+            div_pagination.append(ul);
+            $(self.setting.el).after(div_pagination);//.empty().append(ul);
+        }
+    }
+    function __buildModelObject() {
+        var vue = new Vue({
+            el: self.setting.el,
+            data: {
+                Data: []
+            },
+            methods: self.setting.methods || {}
+        });
+        return vue;
+    }
+    self.Model = __buildModelObject();
+    this.show();
+    return self;
+}
---
+++ YH.ASM.Web/wwwroot/Reference/scripts/xhrUpload.js
@@ -0,0 +1,102 @@
+﻿; (function ($) {
+
+    $.fn.extend({
+        xhrUpload: function (options) {
+            var $all = $(this);
+
+            $all.each(function (index, item) {
+                var $ths = $(item);
+                //console.log($ths,item);
+                new init($ths);
+            });
+            function init($ths) {
+                var self = this;
+                self.unique = 'xhr-' + (new Date()).valueOf();
+                self.selector = "#" + self.unique;
+                $ths.hide();
+                var chosing = options.chosing || function () { return true; }
+                var template = "<div class='xhr-upload pull-left' id='" + self.unique + "' style='display: table; height: 35px; width: 100px;margin-left:20px;margin-top:12px;'>"
+                        + "<div class='xhr-upload-progress' style='border: 1px solid #ccc; height: 100%; display: table-cell; vertical-align: middle;'>"
+                            + "<div class='xhr-upload-procceed' style='background-color: #ffaa00; width: 0%; height: 100%; vertical-align: middle; text-align: right; color: #fff;display:block;'>"
+                                + "<div class='upload-progress-number' style='height:33px;text-align:right;width:100%;line-height:33px;'>0%</div>"
+                            + "</div>"
+                        + "</div>"
+                        + "<div class='xhr-upload-button' style='display: table-cell; height: 100%; width: 32px;'>"
+                    + "<button type='button' name='btn-xhr-file' class='btn btn-sm btn-xhr-file btn-success' style=' display: block; height: 100%; width: 100%;'>"
+                                + "<i class='icon-upload' style='font-size:10px;'> Import</i>"
+                            + "</button>"
+                        + "</div>"
+                    + "</div>";
+                var $ele = $(template);
+                $ele.find(".btn-xhr-file").click(function () {
+                    if (!chosing()) {
+                        return;
+                    }
+                    $ths.click();
+                });
+                $ths.after($ele);
+                var uploadUrl = $ths.attr("data-upload-url");
+                var xhr = new xhrUpload();
+                xhr.progressEventHandler = function (e) {
+                    var percentage = (e.loaded * 100 / e.total).toFixed(2) + "%";
+                    //$ths.parent().find(".xhr-upload-procceed").css({ "width": percentage });
+                    //$ths.parent().find(".xhr-upload-procceed .upload-progress-number").text(percentage)
+                    setProgress(self.selector, percentage);
+                };
+                $ths.change(function () {
+                    setProgress(self.selector, "0%");
+                    var file = $ths[0].files[0];
+                    var customeData = {};
+                    if (typeof options.data == 'function') {
+                        customeData = options.data();
+                    } else {
+                        customeData = options.data;
+                    }
+                    console.log("customeData", customeData);
+                    
+                    var data = $.extend(customeData, { "apk": file });
+
+                    xhr.send(uploadUrl, data, function () {
+                        if (this.readyState === 4) {
+                            //console.log(this);
+                            if (this.status == 200) {
+                                options.completed(eval("(" + this.response + ")"));
+                            } else {
+                                setProgress("0%");
+                                options.completed(null);
+                            }
+                        }
+                    });
+                });
+            }
+            function setProgress(selector, percentage) {
+                $(selector).find(".xhr-upload-procceed").css({ "width": percentage });
+                $(selector).find(".xhr-upload-procceed .upload-progress-number").text(percentage)
+            }
+            function xhrUpload() {
+                this.send = function (url, formJsonData, onload) {
+                    var form = new FormData();
+                    for (var key in formJsonData) {
+                        form.append(key, formJsonData[key]);
+                    }
+                    var xhr = new XMLHttpRequest();
+                    if (xhr.onload) {
+                        xhr.onload = function () { onload.apply(xhr, []); };
+                    } else {
+                        xhr.onreadystatechange = function () { onload.apply(xhr, []); };
+                    }
+                    //xhr.addEventListener("progress", this.progressEventHandler, false);
+                    xhr.upload.addEventListener("progress", this.progressEventHandler, false);
+                    xhr.open("POST", url);
+                    xhr.send(form);
+                };
+                this.progressEventHandler = function (event) {
+
+                }
+            };
+            return self;
+        }
+    });
+
+})(jQuery);
+
