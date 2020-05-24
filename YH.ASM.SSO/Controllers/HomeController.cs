cc88463f7ef25aa762eeb8486ee01143f7207105
274733956 <274733956@qq.com>
2020-05-24 21:10:01 +0800

    引用ids4

--- YH.ASM.SSO/Controllers/HomeController.cs
+++ YH.ASM.SSO/Controllers/HomeController.cs
@@ -11,13 +11,9 @@ namespace YH.ASM.SSO.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+     
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+   
         public IActionResult Index()
         {
             return View();
