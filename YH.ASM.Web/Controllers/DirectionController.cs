13de64273cc907a38c5ad9dd66eb4aeb9df81ec2
demon28 <274733956@qq.com>
2020-06-04 17:52:16 +0800

    新功能开发

--- YH.ASM.Web/Controllers/DirectionController.cs
+++ YH.ASM.Web/Controllers/DirectionController.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace YH.ASM.Web.Controllers
 {
-
+    [Authorize]
     //动向记录
     public class DirectionController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
