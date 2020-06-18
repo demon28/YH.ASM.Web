140706edf47579d6fa33350993f9cc622fb4b2b5
demon28 <274733956@qq.com>
2020-06-18 17:42:34 +0800

    新增创建工单功能

--- YH.ASM.Web/Controllers/SupportController.cs
+++ YH.ASM.Web/Controllers/SupportController.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using YH.ASM.Web.ControllerBase;
 
 namespace YH.ASM.Web.Controllers
 {
+    [Authorize]
     /// <summary>
     /// 工单
     /// </summary>
-    public class SupportController : Controller
+    public class SupportController : ControllerBase.ControllerBase
     {
+        [AuthRight]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AuthRight]
+        public IActionResult Create()
         {
             return View();
         }
